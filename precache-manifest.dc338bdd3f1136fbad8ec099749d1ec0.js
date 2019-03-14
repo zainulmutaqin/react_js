a005650b03f67d61fe707d688f4fc8d460fcd2ba
zainulmutaqin <zainulmutaqin15@gmail.com>
2019-03-14 08:04:30 +0700

    Updates

---
+++ precache-manifest.dc338bdd3f1136fbad8ec099749d1ec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react_js/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "cd027e79261614453ea7",
+    "url": "/react_js/static/js/runtime~main.99506e7c.js"
+  },
+  {
+    "revision": "0d7ad9cd2470b7ee298a",
+    "url": "/react_js/static/js/main.47a5a6a4.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/react_js/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "0d7ad9cd2470b7ee298a",
+    "url": "/react_js/static/css/main.b1144477.chunk.css"
+  },
+  {
+    "revision": "8fed1d6b15943d505ee7dfc486102dba",
+    "url": "/react_js/index.html"
+  }
+];
